92ae408f7b6f15f9a43f68206d9789640ac59418
Hosein Khansari <hrk.x11@gmail.com>
2019-05-01 17:04:14 +0430

    swipe: check slope of every record with its next one, rather than checking first and last ones

--- src/swipe.js
+++ src/swipe.js
@@ -1,8 +1,7 @@
 import router from "./router";
 import { isInput } from "./utils";
 
-/** @type {Array<{ x: number, y: number }>} */
-let records = [];
+/** @type {Array<{ x: number, y: number }>} */ let records = [];
 
 window.addEventListener("touchmove", ({ targetTouches: touches }) => {
 	const [{ clientX: x, clientY: y }] = touches;
@@ -14,13 +13,19 @@ window.addEventListener("touchend", ({ target }) => {
 		const [first] = records,
 			last = records[records.length - 1];
 
-		const d = last.x - first.x;
-		if (d / window.innerWidth > 0.25) {
-			const slope = (last.y - first.y) / (last.x - first.x);
+		const dx = last.x - first.x;
 
-			if (slope < 0.5 && slope > -0.5) {
-				router.push("/");
-			}
+		if (
+			dx / window.innerWidth >= 0.25 &&
+			records
+				.map(({ x, y }, i) => {
+					const next = records[i + 1];
+					return next ? (next.y - y) / (next.x - x) : null;
+				})
+				.filter(i => i !== null)
+				.every(n => Math.abs(n) <= 1.1)
+		) {
+			router.push("/");
 		}
 	}
 	records = [];
