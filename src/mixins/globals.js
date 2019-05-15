d1d9e9b044ede16962b7a5347c980728a2a9cc43
Hosein Khansari <hrk.x11@gmail.com>
2019-05-15 16:42:42 +0430

    mixins: change $reset to exclude given keys

--- src/mixins/globals.js
+++ src/mixins/globals.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 
 Vue.mixin({
 	methods: {
-		$reset() {
-			Object.assign(this.$data, this.$options.data());
+		$reset(...exclude) {
+			const initial = this.$options.data();
+			for (const key in initial) {
+				if (!exclude.includes(key)) {
+					this.$data[key] = initial[key];
+				}
+			}
 		}
 	}
 });
